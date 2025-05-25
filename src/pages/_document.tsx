444ec7430709deb968f4a0eda7d04d639d296140
jdipeer <jdipeer@proton.me>
2025-05-25 05:51:17 +0200

    add google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,13 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-N4PQZR5N"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}></iframe>
+          </noscript>
         </body>
       </Html>
     );
